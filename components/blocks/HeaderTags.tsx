be68a86d688fd0b0b03e66f9ee24cf13df6538d5
Md Nasirul Haque <talk2lurisan@gmail.com>
2024-04-07 23:32:46 +0530

    build fix

--- components/blocks/HeaderTags.tsx
+++ components/blocks/HeaderTags.tsx
@@ -29,7 +29,7 @@ export default function HeaderTags() {
         };
 
         addRemoveClasses();
-        return () => clearTimeout();
+        return () => clearTimeout(1);
     }, []);
 
     return (
