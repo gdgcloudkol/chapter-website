eb6a8affd9b45d11c8837ca6d649e8fa99c17bbe
Gourav Ghosal <slggourav@gmail.com>
2025-03-16 01:53:20 +0530

    added link icon to take to google photos album

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -11,22 +11,53 @@ function page() {
                 {GALLERYDATA.map((item: Gallery) => {
                     return (
                         <>
-                            <Link
-                                href={`${galleryLink}/?id=${item.albumLink}`}
-                                target="_blank"
-                            >
+                            <div>
                                 <div className="p-5 w-80 h-52 border-2 cursor-pointer shadow-lg flex flex-col justify-center">
-                                    <span className="font-bold">
-                                        {item.title}
-                                    </span>
+                                    <div className="flex items-center justify-between">
+                                        <span className="font-bold">
+                                            {item.title}
+                                        </span>
+                                        <Link
+                                            target="_blank"
+                                            href={`https://photos.app.goo.gl/${item.albumLink}`}
+                                        >
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                width="24"
+                                                height="24"
+                                                viewBox="0 0 24 24"
+                                                fill="none"
+                                                stroke="currentColor"
+                                                strokeWidth="2"
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                className="lucide lucide-link-2"
+                                            >
+                                                <path d="M9 17H7A5 5 0 0 1 7 7h2" />
+                                                <path d="M15 7h2a5 5 0 1 1 0 10h-2" />
+                                                <line
+                                                    x1="8"
+                                                    x2="16"
+                                                    y1="12"
+                                                    y2="12"
+                                                />
+                                            </svg>
+                                        </Link>
+                                    </div>
                                     <hr className="mt-2" />
-                                    <img
-                                        className="pt-2 h-36"
-                                        src={item.coverLink}
-                                        alt={item.title}
-                                    />
+                                    <Link
+                                        href={`${galleryLink}/?id=${item.albumLink}`}
+                                        target="_blank"
+                                        className="w-full"
+                                    >
+                                        <img
+                                            className="pt-2 h-36 w-full"
+                                            src={item.coverLink}
+                                            alt={item.title}
+                                        />
+                                    </Link>
                                 </div>
-                            </Link>
+                            </div>
                         </>
                     );
                 })}
